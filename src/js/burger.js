2f425afdf18caf5074cc82da7251a6a213cac323
Kirill Lavrov <zeves095@gmail.com>
2018-08-01 14:44:38 +0300

    update

--- src/js/burger.js
+++ src/js/burger.js
@@ -22,6 +22,54 @@ $.fn.swapWith = function(to) {
   });
 };
 
+function swipedetect(el, callback){
+  
+  var touchsurface = el,
+  swipedir,
+  startX,
+  startY,
+  dist,
+  distX,
+  distY,
+  threshold = 150, //required min distance traveled to be considered swipe
+  restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+  allowedTime = 3000, // maximum time allowed to travel that distance
+  elapsedTime,
+  startTime,
+  handleswipe = callback || function(swipedir){}
+
+  touchsurface.addEventListener('touchstart', function(e){
+      var touchobj = e.changedTouches[0]
+      swipedir = 'none'
+      dist = 0
+      startX = touchobj.pageX
+      startY = touchobj.pageY
+      startTime = new Date().getTime() // record time when finger first makes contact with surface
+      e.preventDefault()
+  }, false)
+
+  touchsurface.addEventListener('touchmove', function(e){
+      e.preventDefault() // prevent scrolling when inside DIV
+  }, false)
+
+  touchsurface.addEventListener('touchend', function(e){
+      var touchobj = e.changedTouches[0]
+      distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+      distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+      elapsedTime = new Date().getTime() - startTime // get time elapsed
+      if (true || elapsedTime <= allowedTime){ // first condition for awipe met
+          if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+              swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+          }
+          else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+              swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+          }
+      }
+      handleswipe(swipedir)
+      e.preventDefault()
+  }, false)
+}
+
 
 var doing = false;
 // var wheel$ = Observable.fromEvent(window, 'wheel')
@@ -44,10 +92,19 @@ $('document').ready(function () {
   // window.addEventListener('wheel', function(e) {
     
   // });
-  $('.slider').each(function(num, item){
-    $('.slider__item',item).click(function(e){
-      const $first = $('.slider__item:nth-child(1)',item);
-      $(this).swapWith($first);
+  $('.slider').each(function(sliderNum, slider){
+    swipedetect(slider, function(swipedir){ });
+    $('.slider__item',slider).each(function(itemNum, item){
+      var itemNumS = itemNum;
+      $(item).click(function(e){
+        if(!itemNum) itemNum = 0;
+        let $rightItems = $('.slider__item:nth-child(n + ' + (itemNum) + ')', slider);
+        let $leftItems = $('.slider__item:nth-child(-n + ' + (itemNum) + ')', slider);
+        $rightItems.removeClass('slider__item--left').addClass('slider__item--right');
+        $leftItems.removeClass('slider__item--right').addClass('slider__item--left');
+        // $('.slider__item:nth-child(n + ' + itemNum + ')', slider).addClass('right');
+        // $('.slider__item:nth-child(' + itemNum + ' - n)', slider).addClass('left');
+      });
     });
   });
 
